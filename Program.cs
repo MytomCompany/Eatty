0a0d01983e2edf7ff461200095cd8d6b2c419cf0
Srimped <nickey200202@gmail.com>
2024-06-14 16:10:56 +0700

    add package

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Eatty.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("EattyDbContextConnection") ?? throw new InvalidOperationException("Connection string 'EattyDbContextConnection' not found.");
+
+builder.Services.AddDbContext<EattyDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<EattyDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
